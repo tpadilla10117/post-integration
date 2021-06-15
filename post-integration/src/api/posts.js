6ebafe9ac4e076e8f8c7d578c365eb8866d5af54
Trinidad Padilla <tpadilla101@live.com>
2021-06-15 12:33:40 -0700

    you can now create posts.  API routing tested

--- post-integration/src/api/posts.js
+++ post-integration/src/api/posts.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { requireUser } = require('./utils');
 const postsRouter = express.Router();
 
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
@@ -20,7 +20,37 @@ postsRouter.get('/', async (req, res) => {
 })
 
 postsRouter.post("/", requireUser, async (req, res, next) => {
-    res.send({ message: 'Under Construction'});
-})
+    const { title, content, tags = "" } = req.body;
+
+    //Spaces removed, then string into an Array
+    //E.g. frontend send "#happy #bloated", tagArr has ["#happy", "#bloated"]
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {};
+
+    //If there are tags, send them over
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+
+    try {
+        postData.title = title;
+        postData.content = content;
+        postData.authorId = req.user.id;
+
+        // this will create the post and the tags for us
+        const post = await createPost(postData);
+        if (post) {
+          res.send({ post });
+        } else {
+          next();
+        }
+
+    } catch ({ name, message }) {
+        next({
+            name: 'MissingContentError',
+            message: 'Missing Title or Content'
+          });
+    }
+});
 
 module.exports = postsRouter;
