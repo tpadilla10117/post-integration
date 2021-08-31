f8bd7752b3b9c9dd852eeca9fff327972ac68a20
Trinidad Padilla <tpadilla101@live.com>
2021-08-30 18:14:12 -0700

    need to troubleshoot why rebuildDb() isnt working in the test

--- src/jest/db_tests/db.spec.js
+++ src/jest/db_tests/db.spec.js
@@ -6,35 +6,45 @@ const { client, getAllUsers, createPost } = require('../../db/index');
 describe('Database', () => {
 /* Before anything, run this code then the tests... */
 /* connect, then rebuild the database... */
+
     beforeAll( async () => {
         /* await client.connect(); */
         await rebuildDB();
     })
+
 /* After the tests run, clean up... */
     afterAll( async() => {
         await client.end();
     })
-    
+  
     describe('users', () => {
         let testUser, testUser2;
         describe('getAllUsers', () => {
             beforeAll(async() => {
-                testUser = await getAllUsers({
-                    id: 1,
-                    username: 'Henryfluff',
-                    name: 'Henry',
-                    location: 'Asgard',
-                    active: true
+                testUser = await getAllUsers({ 
+                    users: [ 
+                        {
+                        id: 1,
+                        username: 'Henryfluff',
+                        name: 'Henry',
+                        location: 'Asgard',
+                        active: true,
+                        }
+                    ]
                 });
-
             })
-            it('user object contains: id, username, name, location, active', async () => {
+            it('user object contains: user: [ {id, username, name, location, active} ]', async () => {
                 expect(testUser).toEqual(expect.objectContaining({
-                    id: expect.any(Number),
-                    username: expect.any(String),
-                    name: expect.any(String),
-                    location: expect.any(String),
-                    active: expect.any(Boolean),
+                    user : [
+                        {
+                            id: expect.any(Number),
+                            username: expect.any(String),
+                            name: expect.any(String),
+                            location: expect.any(String),
+                            active: expect.any(Boolean),
+                        }
+                    ]
+                
                 }))
             })
 
@@ -43,5 +53,5 @@ describe('Database', () => {
 
 })
 
-/*Posts: "authorId", title, content) */
+
 
